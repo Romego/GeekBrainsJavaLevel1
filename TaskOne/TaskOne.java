6be5740c2f66fc2e9f89aa21bd6d64831f016663
romego <romego1992@gmail.com>
2016-10-12 11:55:12 +0300

    Исправил статик

--- TaskOne/TaskOne.java
+++ TaskOne/TaskOne.java
@@ -6,10 +6,10 @@ import java.util.Random;
  */
 class TaskOne {
     static Random rand = new Random();
-    static int[] mas = createArray();
+    int[] mas = createArray();
 
 
-    static int[] createArray(){    // Create an array with random size (5 to 10), but only 1 or 0 value.
+    int[] createArray(){    // Create an array with random size (5 to 10), but only 1 or 0 value.
         int[] newArray = new int[rand.nextInt(5) + 5];
         for (int j = 0 ; j < newArray.length ; j++) {
             newArray[j] = rand.nextInt(2);
@@ -17,16 +17,28 @@ class TaskOne {
         return newArray;
     }
 
-    static int[] swapNumbers(){  // Swap 1 and 0.
+    int[] swapNumbers(){  // Swap 1 and 0.
+
         for (int i = 0 ; i < mas.length; i++){
             mas[i] = (mas[i] == 0) ? 1 : 0;  //add ternary operator
         }
         return mas;
     }
 
-    static void printArrays(){  // print first and second edition of array.
+    void printArrays(){  // print first and second edition of array.
         System.out.println("First edition of Array: " + Arrays.toString(mas));
         System.out.println("Swap edition: " + Arrays.toString(swapNumbers()));
         System.out.println();
+    }
+}
+
+class TaskOneTester {
+    public static void main(String[] args) {
+        TaskOne first = new TaskOne();
+        TaskOne second = new TaskOne();
+        TaskOne third =  new TaskOne();
+        first.printArrays();
+        second.printArrays();
+        third.printArrays();
     }
 }
